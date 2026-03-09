012cb2b2d9d7fc2142c233a5f080c4291ccf2d99
Pratham-Mishra01 <prathamnmishra@gmail.com>
2026-03-09 11:59:57 +0530

    Fixing the file upload error in ui

--- ui/app.py
+++ ui/app.py
@@ -1,7 +1,21 @@
 import streamlit as st
 import requests
+import os
 
-st.title("📄 Research Paper Knowledge Assistant")
+st.title(" Research Paper Knowledge Assistant")
+
+# Upload PDF
+uploaded_file = st.file_uploader("Upload a research paper", type=["pdf"])
+
+if uploaded_file is not None:
+    save_path = os.path.join("data/papers", uploaded_file.name)
+
+    os.makedirs("data/papers", exist_ok=True)
+
+    with open(save_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+
+    st.success(f"File uploaded: {uploaded_file.name}")
 
 # Question input
 question = st.text_input("Ask a question about the research paper")
